8da7908a724f19ffc90a6ef2264c1e4c8449ea0d
Alexandr  Fonari <firstname.lastname@gaetch.edu>
2013-07-14 16:15:13 -0400

    GaAs test - done!

--- test/test_gaas.py
+++ test/test_gaas.py
@@ -6,6 +6,11 @@ import emc
 
 # testing cubic system
 class EMC_Test(unittest.TestCase):
+    def assertListAlmostEqual(self, list1, list2, places, msg):
+        self.assertEqual(len(list1), len(list2))
+        for a, b in zip(list1, list2):
+            self.assertAlmostEqual(float(a), float(b), places, msg)
+
     def setUp(self):
         script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
         inpcar_path = os.path.join(script_dir, "GaAs-5.648-PBE-VASP/INPCAR")
@@ -23,11 +28,13 @@ class EMC_Test(unittest.TestCase):
         self.assertListEqual(basis, [[5.648, 0.0, 0.0],[0.0, 5.648, 0.0],[0.0, 0.0, 5.648]], msg='Failed to parse basis')
 
     def test_calculate_effmass(self):
-        kpt, stepsize, band, prg, basis = emc.parse_inpcar(self.inpcar_fh, debug=False) # will need stepsize later
+        kpt, stepsize, band, prg, basis = emc.parse_inpcar(self.inpcar_fh, debug=False) # will need band and stepsize later
 
         energies = emc.parse_EIGENVAL_VASP(self.eigenval_fh, band, len(emc.diff_d2), debug=False)
-        #m = emc.fd_effmass(energies, stepsize)
-        #self.assertListEqual(m, [[-2.90687, 0.0, 0.0],[0.0, -2.90687, 0.0],[0.0, 0.0, -2.90687]], msg='Failed to parse basis')
+        m = emc.fd_effmass(energies, stepsize, debug=False)
+        self.assertListAlmostEqual(m[0], [-2.90687, 0.0, 0.0], places=5, msg='Failed to calculate effective mass tensor')
+        self.assertListAlmostEqual(m[1], [0.0, -2.90687, 0.0], places=5, msg='Failed to calculate effective mass tensor')
+        self.assertListAlmostEqual(m[2], [0.0, 0.0, -2.90687], places=5, msg='Failed to calculate effective mass tensor')
 
 if __name__ == '__main__':
     unittest.main()
